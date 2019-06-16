81f379174722dddbea1c4bf48688d2b09c58204c
Jagtar Singh <jagtar.ca@gmail.com>
2019-06-16 19:53:05 -0400

    creating sound file and some variables

---
+++ app/src/main/java/com/jagtarkhinda/modernjoust/Sounds.java
@@ -0,0 +1,17 @@
+package com.jagtarkhinda.modernjoust;
+
+import android.content.Context;
+import android.media.SoundPool;
+
+public class Sounds {
+
+    private SoundPool soundPool;
+    private int playerMove;
+    private int playerJumpUp;
+    private int playerJumpDown;
+    private int enemyDie;
+    private int playerDie;
+    private int eggTimeUp;
+    private int collectEgg;
+
+}
